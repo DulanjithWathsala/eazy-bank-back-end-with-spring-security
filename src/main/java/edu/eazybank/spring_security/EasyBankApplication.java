74e0d914a699c5b1627036d8e0973e7ead9c9dd3
M.D.W.De Silva <wdulanjith@gmail.com>
2024-08-25 10:43:41 +0530

    initial commit

---
+++ src/main/java/edu/eazybank/spring_security/EasyBankApplication.java
@@ -0,0 +1,13 @@
+package edu.eazybank.spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eazybank/spring_security/EasyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eazybank.spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
