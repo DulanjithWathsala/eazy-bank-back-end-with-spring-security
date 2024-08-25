597f4145ff9659f07caabd090e9e875cfdd0aba0
M.D.W.De Silva <wdulanjith@gmail.com>
2024-08-25 18:06:59 +0530

    bcrypt password encoded added to admin user

--- src/main/java/edu/eazybank/spring_security/config/ProjectSecurityConfig.java
+++ src/main/java/edu/eazybank/spring_security/config/ProjectSecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -26,8 +28,19 @@ public class ProjectSecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user").password("{noop}12345").authorities("read").build();
-        UserDetails admin = User.withUsername("admin").password("{noop}54321").authorities("admin").build();
+        UserDetails user = User.withUsername("user")
+                .password("{noop}12345")
+                .authorities("read")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password("{bcrypt}$2a$12$DE.ujz8eGAEMpakUjOWJ9uKVyuTAEAi0/G26szRw1KWj4HRaBUgi6")
+                .authorities("admin")
+                .build();
         return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
