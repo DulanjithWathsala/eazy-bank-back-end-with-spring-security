cf06cba077c3e686a15789ca1367671ca239a32e
M.D.W.De Silva <wdulanjith@gmail.com>
2024-08-28 11:35:38 +0530

    enhanced the application to use authentication from mysql database

--- src/main/java/edu/eazybank/spring_security/config/ProjectSecurityConfig.java
+++ src/main/java/edu/eazybank/spring_security/config/ProjectSecurityConfig.java
@@ -4,14 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -29,16 +29,8 @@ public class ProjectSecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user")
-                .password("{noop}EazyBank@12345")
-                .authorities("read")
-                .build();
-        UserDetails admin = User.withUsername("admin")
-                .password("{bcrypt}$2a$12$HOexDOGUtcpu.YXCqp.cEePl4cYdu8UfKjvRbot.Guiwpo5i3vqna")
-                .authorities("admin")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
