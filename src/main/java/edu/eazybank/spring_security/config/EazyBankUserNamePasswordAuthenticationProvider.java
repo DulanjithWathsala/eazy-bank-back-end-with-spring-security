64e79e798e8ff41b86c0fa8e84a08a04394da2c7
M.D.W.De Silva <wdulanjith@gmail.com>
2024-09-03 15:16:48 +0530

    removed unused imports

--- src/main/java/edu/eazybank/spring_security/config/EazyBankUserNamePasswordAuthenticationProvider.java
+++ src/main/java/edu/eazybank/spring_security/config/EazyBankUserNamePasswordAuthenticationProvider.java
@@ -3,7 +3,6 @@ package edu.eazybank.spring_security.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
