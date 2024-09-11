c9962c5514cfa4661d30253770fb686222280dea
M.D.W.De Silva <wdulanjith@gmail.com>
2024-09-11 14:44:39 +0530

    card controller updated

--- src/main/java/edu/eazybank/spring_security/controller/CardsController.java
+++ src/main/java/edu/eazybank/spring_security/controller/CardsController.java
@@ -1,15 +1,28 @@
 package edu.eazybank.spring_security.controller;
 
+import edu.eazybank.spring_security.entity.CardEntity;
+import edu.eazybank.spring_security.repository.CardRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/cards")
+@RequiredArgsConstructor
 public class CardsController {
 
-    @GetMapping()
-    public String getContactDetails() {
-        return "eazy bank cards details";
+    private final CardRepository cardsRepository;
+
+    @GetMapping("/myCards")
+    public List<CardEntity> getCardDetails(@RequestParam long id) {
+        List<CardEntity> cards = cardsRepository.findByCustomerId(id);
+        if (cards != null ) {
+            return cards;
+        }else {
+            return null;
+        }
     }
 }
