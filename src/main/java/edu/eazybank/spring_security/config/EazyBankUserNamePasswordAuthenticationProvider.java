67d7c7740c0ca84bdc269cf3a66da0353a4f23df
M.D.W.De Silva <wdulanjith@gmail.com>
2024-09-11 13:10:13 +0530

    test scripts added to add test data to database

--- src/main/java/edu/eazybank/spring_security/config/EazyBankUserNamePasswordAuthenticationProvider.java
+++ src/main/java/edu/eazybank/spring_security/config/EazyBankUserNamePasswordAuthenticationProvider.java
@@ -8,7 +8,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
