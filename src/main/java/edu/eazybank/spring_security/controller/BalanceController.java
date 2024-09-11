22d21a4d081fd0e51a29f12c051cf52f77dd21b0
M.D.W.De Silva <wdulanjith@gmail.com>
2024-09-11 14:41:20 +0530

    balance controller updated

--- src/main/java/edu/eazybank/spring_security/controller/BalanceController.java
+++ src/main/java/edu/eazybank/spring_security/controller/BalanceController.java
@@ -1,15 +1,29 @@
 package edu.eazybank.spring_security.controller;
 
+import edu.eazybank.spring_security.entity.AccountTransactionEntity;
+import edu.eazybank.spring_security.repository.AccountTransactionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/balance")
+@RequiredArgsConstructor
 public class BalanceController {
 
-    @GetMapping()
-    public String getContactDetails() {
-        return "eazy bank balance details";
+    private final AccountTransactionRepository accountTransactionsRepository;
+
+    @GetMapping("/myBalance")
+    public List<AccountTransactionEntity> getBalanceDetails(@RequestParam long id) {
+        List<AccountTransactionEntity> accountTransactions = accountTransactionsRepository.
+                findByCustomerIdOrderByTransactionDtDesc(id);
+        if (accountTransactions != null) {
+            return accountTransactions;
+        } else {
+            return null;
+        }
     }
 }
