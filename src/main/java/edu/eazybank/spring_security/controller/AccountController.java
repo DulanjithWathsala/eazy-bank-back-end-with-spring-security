7e022fbb53cc278fe014621e1e3b474f523d7335
M.D.W.De Silva <wdulanjith@gmail.com>
2024-09-11 14:39:26 +0530

    account controller updated

--- src/main/java/edu/eazybank/spring_security/controller/AccountController.java
+++ src/main/java/edu/eazybank/spring_security/controller/AccountController.java
@@ -1,15 +1,26 @@
 package edu.eazybank.spring_security.controller;
 
+import edu.eazybank.spring_security.entity.AccountEntity;
+import edu.eazybank.spring_security.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/account")
+@RequiredArgsConstructor
 public class AccountController {
 
-    @GetMapping()
-    public String getContactDetails() {
-        return "eazy bank account details";
+    private final AccountRepository accountsRepository;
+
+    @GetMapping("/myAccount")
+    public AccountEntity getAccountDetails(@RequestParam long id) {
+        AccountEntity accounts = accountsRepository.findByCustomerId(id);
+        if (accounts != null) {
+            return accounts;
+        } else {
+            return null;
+        }
     }
 }
