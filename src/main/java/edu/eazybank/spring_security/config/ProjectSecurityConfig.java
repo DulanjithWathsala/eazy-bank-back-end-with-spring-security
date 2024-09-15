4be82ae3f34f21490b2b34e8e3c7a3940d86b8f2
M.D.W.De Silva <wdulanjith@gmail.com>
2024-09-15 10:31:21 +0530

    Cross-Origin Resource Sharing(cors) configured

--- src/main/java/edu/eazybank/spring_security/config/ProjectSecurityConfig.java
+++ src/main/java/edu/eazybank/spring_security/config/ProjectSecurityConfig.java
@@ -7,10 +7,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -21,10 +25,19 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(corsConfig -> corsConfig.configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                    config.setAllowedMethods(Collections.singletonList("*"));
+                    config.setAllowCredentials(true);
+                    config.setAllowedHeaders(Collections.singletonList("*"));
+                    config.setMaxAge(3600L);
+                    return config;
+                }))
                 .sessionManagement(sessionManagementConfig -> sessionManagementConfig.invalidSessionUrl("/invalidSession")
                                 .maximumSessions(5).maxSessionsPreventsLogin(true))
                 .requiresChannel(requiresChannelConfig -> requiresChannelConfig.anyRequest().requiresInsecure()) // Only accept HTTP requests
-                .csrf(csrfConfig -> csrfConfig.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests -> requests
                 .requestMatchers("/account", "/balance", "/cards", "/loans", "/user").authenticated()
                 .requestMatchers("/contact", "/notices", "/register", "/error", "invalidSession").permitAll());
--- src/main/java/edu/eazybank/spring_security/config/ProjectSecurityProdConfig.java
+++ src/main/java/edu/eazybank/spring_security/config/ProjectSecurityProdConfig.java
@@ -7,10 +7,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -21,10 +25,19 @@ public class ProjectSecurityProdConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(corsConfig -> corsConfig.configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                    config.setAllowedMethods(Collections.singletonList("*"));
+                    config.setAllowCredentials(true);
+                    config.setAllowedHeaders(Collections.singletonList("*"));
+                    config.setMaxAge(3600L);
+                    return config;
+                }))
                 .sessionManagement(sessionManagementConfig -> sessionManagementConfig.invalidSessionUrl("/invalidSession")
                         .maximumSessions(1).maxSessionsPreventsLogin(true))
                 .requiresChannel(requiresChannelConfig -> requiresChannelConfig.anyRequest().requiresSecure()) // Only accept HTTPS requests
-                .csrf(csrfConfig -> csrfConfig.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests -> requests
                 .requestMatchers("/account", "/balance", "/cards", "/loans").authenticated()
                 .requestMatchers("/contact", "/notices", "/user/register", "/error", "/invalidSession").permitAll());
